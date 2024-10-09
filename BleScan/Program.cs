6976d51d38aad73bb7efa46add8d0a1001456b63
usausa <machi.pon@gmail.com>
2024-10-09 19:23:13 +0900

    Add project

---
+++ BleScan/Program.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+var watcher = new BluetoothLEAdvertisementWatcher
+{
+    ScanningMode = BluetoothLEScanningMode.Passive
+};
+
+watcher.Received += WatcherOnReceived;
+
+// TODO
+// Watcherのオプション
+// 1度だけ
+// Deviceが必要なこと
+// GATTの詳細表示
+
+async void WatcherOnReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+{
+    // TODO ロック
+    // TODO 基本情報で出せること
+    Debug.WriteLine($"Address: {args.BluetoothAddress:X12}");
+
+    // Device
+    var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+
+    // Gatt
+    var gatt = device is not null ? await device.GetGattServicesAsync() : null;
+    //if (gatt?.Services.Count > 0)
+    //{
+    //    for (var i = 0; i < gatt!.Services.Count; i++)
+    //    {
+    //        Console.WriteLine($"  {gatt.Services[i].Uuid}");
+    //    }
+    //}
+
+    Console.WriteLine($"{args.Timestamp:HH:mm:ss.fff} {args.BluetoothAddress:X12} {args.RawSignalStrengthInDBm} {device?.Name} {gatt?.Status} {gatt?.Services.Count}");
+}
+
+watcher.Start();
+
+Console.ReadLine();
